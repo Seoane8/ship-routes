3df32b26454ee21e1143626edc01783f011180fc
Diego Seoane González <diegoseoanegonzalez8@gmail.com>
2023-01-01 17:07:04 +0100

    feat(ships): add endpoint to record ship

--- apps/main/com/shiproutes/apps/ships/backend/config/ShipsServerConfiguration.java
+++ apps/main/com/shiproutes/apps/ships/backend/config/ShipsServerConfiguration.java
@@ -2,16 +2,20 @@ package com.shiproutes.apps.ships.backend.config;
 
 import com.shiproutes.apps.ships.backend.middleware.BasicHttpAuthMiddleware;
 import com.shiproutes.shared.domain.bus.command.CommandBus;
+import com.shiproutes.shared.infrastructure.spring.ApiExceptionMiddleware;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
 public class ShipsServerConfiguration {
     private final CommandBus bus;
+    private final RequestMappingHandlerMapping mapping;
 
-    public ShipsServerConfiguration(CommandBus bus) {
+    public ShipsServerConfiguration(CommandBus bus, RequestMappingHandlerMapping mapping) {
         this.bus = bus;
+        this.mapping = mapping;
     }
 
     @Bean
@@ -20,6 +24,15 @@ public class ShipsServerConfiguration {
 
         registrationBean.setFilter(new BasicHttpAuthMiddleware(bus));
         registrationBean.addUrlPatterns("/health-check");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ApiExceptionMiddleware> apiExceptionMiddleware() {
+        FilterRegistrationBean<ApiExceptionMiddleware> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new ApiExceptionMiddleware(mapping));
 
         return registrationBean;
     }
