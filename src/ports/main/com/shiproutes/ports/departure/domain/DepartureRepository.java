cc4be719203442417efeb5f8f1d0538ea0eae3a2
Diego Seoane González <diegoseoanegonzalez8@gmail.com>
2023-01-09 17:43:26 +0100

    feat(ports): add implementation of DepartureRepository

--- src/ports/main/com/shiproutes/ports/departure/domain/DepartureRepository.java
+++ src/ports/main/com/shiproutes/ports/departure/domain/DepartureRepository.java
@@ -1,7 +1,11 @@
 package com.shiproutes.ports.departure.domain;
 
+import java.util.Optional;
+
 public interface DepartureRepository {
 
     void save(Departure departure);
+
+    Optional<Departure> search(DepartureId id);
 
 }
