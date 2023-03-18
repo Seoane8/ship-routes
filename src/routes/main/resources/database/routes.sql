09b12b984c7352bec2c771905f7ca3510e00ae88
Diego Seoane González <diegoseoanegonzalez8@gmail.com>
2023-03-18 12:37:50 +0100

    feature(routes): add route entity

--- src/routes/main/resources/database/routes.sql
+++ src/routes/main/resources/database/routes.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE IF NOT EXISTS routes
+(
+    id            CHAR(36) NOT NULL,
+    departurePort CHAR(36) NOT NULL,
+    arrivalPort   CHAR(36) NOT NULL,
+    distance      INT      NOT NULL,
+    PRIMARY KEY (id)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
