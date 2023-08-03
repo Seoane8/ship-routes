f4a14631a5dbd8866bd8f2c0a17faa3c0001fa05
Diego Seoane <diegosgo@ext.inditex.com>
2023-08-03 22:18:15 +0200

    fix: fix small issues

--- src/backoffice/main/com/shiproutes/backoffice/port/application/ingest/PortIngestor.java
+++ src/backoffice/main/com/shiproutes/backoffice/port/application/ingest/PortIngestor.java
@@ -33,8 +33,8 @@ public class PortIngestor {
 
         PortIngestedEvent event = new PortIngestedEvent(
             port.id().value(),
-            port.locode(),
             name,
+            port.locode(),
             latitude,
             longitude
         );
